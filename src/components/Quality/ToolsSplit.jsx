bb39bf822c67e617bf7de5a418f09b1bc6097df9
Jay-kanani18 <jkanani.elluminati@gmail.com>
2026-01-23 15:00:43 +0530

    fix bugs

--- src/components/Quality/ToolsSplit.jsx
+++ src/components/Quality/ToolsSplit.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const ToolsSplit = () => {
   return (
-    <section className="about section-padding">
+    <section className="tools-split section-padding">
       <div className="container">
-        <div className="row">
+        <div className="row align-items-center">
+
+          {/* LEFT CONTENT */}
           <div className="col-lg-5">
-            <div className="extra wow fadeInUp" data-wow-delay=".3s">
-              <h2 className="mb-20 playfont fz-50">Precision Measurement & Metrology</h2>
+            <div className="tools-content wow fadeInUp" data-wow-delay=".3s">
+              <h2 className="mb-20 playfont">
+                Precision Measurement & Metrology
+              </h2>
               <p>
                 State-of-the-art measurement tools and inspection systems ensure
                 every dimension is verified to specification before it leaves our floor.
@@ -18,19 +22,22 @@ const ToolsSplit = () => {
               </p>
             </div>
           </div>
-          <div className="col-lg-4 valign">
-            <div className="specialty wow fadeInUp" data-wow-delay=".5s">
+
+          {/* RIGHT LIST */}
+          <div className="col-lg-6 offset-lg-1">
+            <div className="tools-list wow fadeInUp" data-wow-delay=".5s">
               <ul>
-                <li><span>01.</span> Facing & Milling Machine Inspection</li>
-                <li><span>02.</span> Digital & Mitutoyo Vernier / Dial Vernier</li>
-                <li><span>03.</span> Digital Micro Meters</li>
-                <li><span>04.</span> Dial & Snap Gauges</li>
-                <li><span>05.</span> Special I.D Pin Gauges</li>
-                <li><span>06.</span> Thread Plug & Ring Gauges</li>
-                <li><span>07.</span> Magnifying Eye Glass for QC</li>
+                <li><span>01</span> Facing & Milling Machine Inspection</li>
+                <li><span>02</span> Digital & Mitutoyo Vernier / Dial Vernier</li>
+                <li><span>03</span> Digital Micro Meters</li>
+                <li><span>04</span> Dial & Snap Gauges</li>
+                <li><span>05</span> Special I.D Pin Gauges</li>
+                <li><span>06</span> Thread Plug & Ring Gauges</li>
+                <li><span>07</span> Magnifying Eye Glass for QC</li>
               </ul>
             </div>
           </div>
+
         </div>
       </div>
     </section>
