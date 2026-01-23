7b50f8636693202a4bbd8500d88de36008cc41e3
Jay-kanani18 <jkanani.elluminati@gmail.com>
2026-01-23 18:39:29 +0530

    add powered by

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,77 +7,44 @@ const Footer = ({ classText }) => {
     <footer className={`${classText ? classText : ""}`}>
       <div className="container">
         <div className="row">
+          {/* Left */}
           <div className="col-lg-4">
             <div className="item">
               <div className="logo">
-                <img src="/assets/img/Logo/SVG/Dark BG Logo.svg" width={200} height={200} alt="" />
+                <img
+                  src="/assets/img/Logo/SVG/Dark BG Logo.svg"
+                  width={200}
+                  height={200}
+                  alt=""
+                />
               </div>
               <div className="social">
-                <Link href="#">
-                  <a>
-                    <i className="fab fa-facebook-f"></i>
-                  </a>
-                </Link>
-                <Link href="#">
-                  <a>
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                </Link>
-                <Link href="#">
-                  <a>
-                    <i className="fab fa-instagram"></i>
-                  </a>
-                </Link>
-                <Link href="#">
-                  <a>
-                    <i className="fab fa-youtube"></i>
-                  </a>
-                </Link>
+                <Link href="#"><a><i className="fab fa-facebook-f"></i></a></Link>
+                <Link href="#"><a><i className="fab fa-twitter"></i></a></Link>
+                <Link href="#"><a><i className="fab fa-instagram"></i></a></Link>
+                <Link href="#"><a><i className="fab fa-youtube"></i></a></Link>
               </div>
-
-
             </div>
           </div>
+
+          {/* Center */}
           <div className="col-lg-4">
             <div className="item md-mb50">
               <div className="title">
                 <h5>Quick Links</h5>
               </div>
               <ul>
-                <li>
-                  <Link href="/">
-                    <a>Home</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/about">
-                    <a>About Us</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/products">
-                    <a>Products</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/quality">
-                    <a>Quality</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/brochure">
-                    <a>Brochure</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/contact">
-                    <a>Contact Us</a>
-                  </Link>
-                </li>
-
+                <li><Link href="/"><a>Home</a></Link></li>
+                <li><Link href="/about"><a>About Us</a></Link></li>
+                <li><Link href="/products"><a>Products</a></Link></li>
+                <li><Link href="/quality"><a>Quality</a></Link></li>
+                <li><Link href="/brochure"><a>Brochure</a></Link></li>
+                <li><Link href="/contact"><a>Contact Us</a></Link></li>
               </ul>
             </div>
           </div>
+
+          {/* Right */}
           <div className="col-lg-4">
             <div className="item md-mb50">
               <div className="title">
@@ -88,8 +55,8 @@ const Footer = ({ classText }) => {
                   <span className="icon pe-7s-map-marker"></span>
                   <div className="cont">
                     <h6>Office Address</h6>
-                    <p>PLOT NO. 3092  GIDC Phase 2, </p>
-                    <p>Jamngar, Gujarat - 361006</p>
+                    <p>PLOT NO. 3092 GIDC Phase 2,</p>
+                    <p>Jamnagar, Gujarat - 361006</p>
                   </div>
                 </li>
                 <li>
@@ -109,14 +76,30 @@ const Footer = ({ classText }) => {
               </ul>
             </div>
           </div>
-
-
-
         </div>
-        <div className="copy-right text-center mt-50">
-          <p>© 2026 Maruti Brass Industry.  All rights reserved. </p>
 
+        {/* Bottom Bar */}
+        <div className="copy-right mt-50">
+          <div className="row align-items-center">
+            <div className="col-md-6 text-center text-md-left">
+              <p>© 2026 Maruti Brass Industry. All rights reserved.</p>
+            </div>
+
+            <div className="col-md-6 text-center text-md-right">
+              <p className="powered-by">
+                Powered by{" "}
+                <a
+                  href="https://axorainfotech.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Axora Infotech
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
+
       </div>
     </footer>
   );
